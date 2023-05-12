b82734ee52047dd0b509d35215ea2715f562a4fe
diogotavc <diogotavc@protonmail.com>
2023-05-12 15:59:50 +0100

    server.py: update_file: Corrigir um erro no código

--- client_server/server.py
+++ client_server/server.py
@@ -240,12 +240,12 @@ def create_file():
 # update report csv file with the simulation of the client
 def update_file(client_id, size, guess):
 	# Variables used for debugging before everything is fully implemented
-	size = size if size else "None"
+	size = size if size else 0
 	guess = guess if guess else []
 
 	with open("result.csv", "a") as csvfile:
 		write = csv.writer(csvfile, delimiter=',')
-		csv.writer.writerow([client_id], size, guess)
+		write.writerow([client_id, size, guess])
 
 
 #
